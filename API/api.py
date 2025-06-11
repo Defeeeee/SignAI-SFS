b86f4787aba58e47836480790823c83f5467e76a
Federico Diaz Nemeth <fdiaznemeth@gmail.com>
2025-06-10 22:21:32 -0300

    feat(api): update CORS middleware to allow all origins
    Signed-off-by: Federico Diaz Nemeth <fdiaznemeth@gmail.com>

--- API/api.py
+++ API/api.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.add_middleware(
-    CORSMiddleware, allow_origins=['null'],
+    CORSMiddleware, allow_origins=['*'],
     allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
 
 @app.get("/")
